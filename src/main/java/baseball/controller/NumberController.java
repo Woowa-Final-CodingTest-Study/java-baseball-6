147bad442148c2d055886469b62fdaec16dd3a1c
Chenowoo <cjsgusdn14@naver.com>
2023-11-23 16:51:18 +0900

    feat(NumberController): 컴퓨터가 서로 다른 세개의 수를 선택하는 기능 구현

---
+++ src/main/java/baseball/controller/NumberController.java
@@ -0,0 +1,22 @@
+package baseball.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberController {
+
+    public List<Integer> createComputerNumbers() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int pickRandomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (randomNumbers.contains(pickRandomNumber)) {
+                continue;
+            }
+            randomNumbers.add(pickRandomNumber);
+        }
+        return randomNumbers;
+    }
+}
