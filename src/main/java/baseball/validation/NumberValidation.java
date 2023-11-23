d980866eab6428b6651be426226f675f2d6fbce9
Chenowoo <cjsgusdn14@naver.com>
2023-11-23 16:51:44 +0900

    feat(NumberValidation): 입력 예외 기능 구현

---
+++ src/main/java/baseball/validation/NumberValidation.java
@@ -0,0 +1,59 @@
+package baseball.validation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumberValidation {
+
+    public void validateInputNumber(String input) {
+        validateNull(input);
+        validateIntRange(input);
+        validateInputForm(input);
+        validateDuplicateNumbers(input);
+    }
+
+    public void validateInputReStartNumber(String input){
+        validateNull(input);
+        validateIntRange(input);
+        validateInputForm(input);
+        validateInputReStartNumber(input);
+    }
+
+    public void validateNull(String input) {
+        if (input == "" || input.equals(null)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateIntRange(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateInputForm(String input) {
+        String deleteExceptNumber = input.replaceAll("[0-9]", "");
+
+        if (!deleteExceptNumber.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateDuplicateNumbers(String input) {
+        Set<String> numbers = new HashSet<>();
+
+        for (String number : input.split("")) {
+            if (!numbers.add(number)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void validateReStartNumber(String input) {
+        if (input.equals("1") && input.equals("2")){
+            throw new IllegalArgumentException();
+        }
+    }
+}
