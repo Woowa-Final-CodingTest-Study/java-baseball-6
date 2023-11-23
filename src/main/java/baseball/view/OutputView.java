25f7d4f25fb97352b1d43c4eaf7ebcf143fa6253
Chenowoo <cjsgusdn14@naver.com>
2023-11-23 16:49:56 +0900

    refactor(OutputView): 출력 메시지 분리

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,35 +1,35 @@
 package baseball.view;
 
-import baseball.constant.CountConstant;
-import baseball.constant.MessageConstant;
+import static baseball.constant.CountConstant.*;
+import static baseball.constant.MessageConstant.*;
 
 public class OutputView {
 
     public void printGameStart() {
-        System.out.println(MessageConstant.GAME_START_MESSAGE.getMessage());
+        System.out.println(GAME_START_MESSAGE.getMessage());
     }
 
     public void printInputNumber() {
-        System.out.print(MessageConstant.INPUT_NUMBER_MESSAGE.getMessage());
+        System.out.print(INPUT_NUMBER_MESSAGE.getMessage());
     }
 
     public void printCount(int strike, int ball) {
         if (strike == 0) {
-            System.out.println();
-            System.out.printf(CountConstant.BALL.getCount(), ball);
+            System.out.printf(BALL.getCount(), ball);
             return;
         }
         if (ball == 0) {
-            System.out.println();
-            System.out.printf(CountConstant.STRIKE.getCount(), strike);
+            System.out.printf(STRIKE.getCount(), strike);
             return;
         }
-        System.out.println();
-        System.out.printf(CountConstant.BALL_STRIKE.getCount(), ball, strike);
+        System.out.printf(BALL_STRIKE.getCount(), ball, strike);
     }
 
     public void printGameOver() {
-        System.out.println();
-        System.out.println(MessageConstant.GAME_OVER_MESSAGE.getMessage());
+        System.out.println(GAME_OVER_MESSAGE.getMessage());
+    }
+
+    public void printAskReStart(){
+        System.out.println(ASK_RESTART.getMessage());
     }
 }
