db407f10d7ccbbcbe3d5e595d04b5b9095f36f39
YoonseolLee <thisyoon97@gmail.com>
2023-11-24 23:51:50 +0900

    feat: GameService 생성

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,46 +1,12 @@
 package baseball.controller;
 
-import baseball.domain.BallStrikeCount;
-import baseball.domain.Computer;
-import baseball.domain.RandomNumberGenerator;
-import baseball.domain.User;
-import baseball.view.InputView;
-import baseball.view.OutputView;
+import baseball.domain.GameService;
 
 public class Controller {
-    public static final String RESTART = "1";
-    public static final String EXIT = "2";
-
-    InputView inputView = new InputView();
-    RandomNumberGenerator generator = new RandomNumberGenerator();
+    GameService gameService = new GameService();
 
     public void run() {
-        start();
-        repeatGame();
-    }
-
-    public void start() {
-        OutputView.printStartGame();
-    }
-
-    public void repeatGame() {
-        Computer computer = generator.play();
-        computer.printComputerNumbers();
-        BallStrikeCount ballStrikeCount;
-        do {
-            User userNumbers = inputView.getUserInput();
-            ballStrikeCount = computer.calculateBallCount(userNumbers);
-            OutputView.printResult(ballStrikeCount);
-        } while (ballStrikeCount.getStrikeCount() != 3);
-        OutputView.printGameOver();
-        showEnding();
-    }
-
-    public void showEnding() {
-        OutputView.printEnding();
-        String answer = InputView.askRestartOrExit();
-        if (answer.equals(RESTART)) {
-            repeatGame();
-        }
+        gameService.start();
+        gameService.repeatGame();
     }
 }
---
+++ src/main/java/baseball/domain/GameService.java
@@ -0,0 +1,37 @@
+package baseball.domain;
+
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class GameService {
+    public static final String RESTART = "1";
+    public static final String EXIT = "2";
+
+    InputView inputView = new InputView();
+    RandomNumberGenerator generator = new RandomNumberGenerator();
+
+    public void start() {
+        OutputView.printStartGame();
+    }
+
+    public void repeatGame() {
+        Computer computer = generator.play();
+        computer.printComputerNumbers();
+        BallStrikeCount ballStrikeCount;
+        do {
+            User userNumbers = inputView.getUserInput();
+            ballStrikeCount = computer.calculateBallCount(userNumbers);
+            OutputView.printResult(ballStrikeCount);
+        } while (ballStrikeCount.getStrikeCount() != 3);
+        OutputView.printGameOver();
+        showEnding();
+    }
+
+    public void showEnding() {
+        OutputView.printEnding();
+        String answer = InputView.askRestartOrExit();
+        if (answer.equals(RESTART)) {
+            repeatGame();
+        }
+    }
+}
